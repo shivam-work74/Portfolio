c07a39a3f675b376b4fc28c35df2fa2f3bc1f4f5
shivam-work74 <shivam.work7488@gmail.com>
2025-08-14 16:24:22 +0530

    some basic changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import About from "./components/About";
+
+
 import Contact from "./components/Contact";
 
 function App() {

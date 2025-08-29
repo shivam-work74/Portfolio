6651985c84ea50057271b47c308afd29abbc37d1
Shivam <shivam.work7488@gmail.com>
2025-08-29 16:19:33 +0530

    Update ProjectsPage.js

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -60,7 +60,7 @@ const projects = [
     title: "Fools-Fortune",
     description: "A interesting and unique game of cards.",
     github: "https://github.com/shivam-work74/Fools-Fortune.git",
-    demo: "https://fools-fortune-z47n.vercel.app/",
+    demo: "https://fools-fortune-rqzq.vercel.app/",
     image: "/fortune.png",
     tags: ["React", "API"],
   },
